d7f22027086bb0eeebc622200b21e9769b167ff7
MURUGAN G <murugankgm25@gmail.com>
2026-06-22 20:59:13 +0530

    Time: 1 ms (97.88%), Space: 42.6 MB (41.82%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,22 @@
+class Solution {
+
+public    int mySqrt(int x) {
+        // int odd = 1, n = 0;
+
+        // // itrate till x becomes 0 or negative
+        // while (x > 0) {
+        //     x -= odd;
+        //     odd += 2; // next odd number on each iteration, odd = 1, 3, 5,...
+        //     n++;
+        // }
+
+        // // if x is a perfect suare then subtracting n odd numbers would result in 0
+        // if (x == 0) {
+        //     return n;
+        // }
+
+        // // if x is not a perfect square then 
+        // return n - 1;
+        return (int)Math.sqrt(x);
+    }
+}
