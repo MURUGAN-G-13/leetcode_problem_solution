96afab005b79f3cfa379e79c6a1fc0a2a05cf0a6
MURUGAN G <murugankgm25@gmail.com>
2026-07-06 23:12:37 +0530

    Time: 0 ms (100%), Space: 44.2 MB (97.28%) - LeetHub

--- 3688-bitwise-or-of-even-numbers-in-an-array/3688-bitwise-or-of-even-numbers-in-an-array.java
+++ 3688-bitwise-or-of-even-numbers-in-an-array/3688-bitwise-or-of-even-numbers-in-an-array.java
@@ -2,7 +2,7 @@ class Solution {
     public int evenNumberBitwiseORs(int[] nums) {
         int evensum=0;
             for(int i:nums){
-                if((i&1)==0){
+                if((i%2)==0){
                     evensum |= i;
                 }
             }return evensum;
