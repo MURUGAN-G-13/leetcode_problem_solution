0f74ce23d19be951b2c5ad920954f0cb1fc2b5fb
MURUGAN G <murugankgm25@gmail.com>
2026-07-19 21:12:13 +0530

    Time: 2 ms (68.04%), Space: 42.8 MB (83.35%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.java
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -1,18 +1,17 @@
 
 class Solution {
     public int countAsterisks(String s) {
-        int star = 0;
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '|') {
+        int star=0;
+        int count=0;
+        for (int i=0;i<s.length();i++) {
+            char c= s.charAt(i);
+            if (c== '|') {
                 star++;
             }
-            if (c == '*' && star % 2 == 0) {
+            if (c== '*' && star%2== 0) {
                 count++;
             }
         }
-
         return count;
     }
 }
