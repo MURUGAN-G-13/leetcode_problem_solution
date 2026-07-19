de5541c15e9818d9354a7237b7e4e07519317bbf
MURUGAN G <murugankgm25@gmail.com>
2026-07-19 21:09:26 +0530

    Time: 2 ms (68.04%), Space: 43 MB (56.09%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.java
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -1,9 +1,3 @@
-public class Main {
-    public static void main(String[] args) {
-        Solution obj = new Solution();
-        System.out.println(obj.countAsterisks("l|*e*et|c**o|*de|"));
-    }
-}
 
 class Solution {
     public int countAsterisks(String s) {
