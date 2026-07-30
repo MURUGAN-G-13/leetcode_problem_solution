3a3efbd0de7841ffd2af436dfcb6ea09a764beae
MURUGAN G <murugankgm25@gmail.com>
2026-07-30 22:34:16 +0530

    Time: 0 ms (100%), Space: 43.1 MB (79.43%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -3,15 +3,16 @@ class Solution {
         int sum=0;
         int len=word.length();
         for(int i=0;i<len;i++){
-            if(i<=7){
-                sum+=1;
-            }else if(i<=15){
-                sum+=2;
-            }else if(i<=23){
-                sum+=3;
-            }else{
-                sum+=4;
-            }
+            // if(i<=7){
+            //     sum+=1;
+            // }else if(i<=15){
+            //     sum+=2;
+            // }else if(i<=23){
+            //     sum+=3;
+            // }else{
+            //     sum+=4;
+            // }
+            sum =sum+ (i/8)+1;
         }return sum;
     }
 }
